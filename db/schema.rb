0e83ab8cc9f6107abc60cbc402a317e2147c5388
hokatsu <46119001+hokatsu@users.noreply.github.com>
2020-07-03 18:29:33 +0900

    imageカラムの削除
    rails db:rollbackでやり直しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_03_074329) do
+ActiveRecord::Schema.define(version: 2020_07_03_073609) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"

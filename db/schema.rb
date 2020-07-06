7c41fd2a429ed02cef213540d757ef78826976af
hokatsu <46119001+hokatsu@users.noreply.github.com>
2020-07-06 18:49:12 +0900

    nicknameカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_07_06_061016) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"

292b6440d751bbf0aa5491c26c20a555c155bd55
hokatsu <46119001+hokatsu@users.noreply.github.com>
2020-07-07 15:14:36 +0900

    外部キー制約を設定したuser_idカラムを追加

--- db/migrate/20200703073609_create_tweets.rb
+++ db/migrate/20200703073609_create_tweets.rb
@@ -2,8 +2,8 @@ class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
       t.string :text
-      t.references :user
       t.timestamps
+      t.references :user, foreign_key: true
     end
   end
 end

41cfaae5c2a84dd43f09f7b3180496bf43960c6c
hokatsu <46119001+hokatsu@users.noreply.github.com>
2020-07-07 11:17:11 +0900

    tweetsテーブルにuser_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2020_07_06_061016) do
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "text"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

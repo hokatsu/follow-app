0befbd322eece7c55be255975afbba00046b2434
hokatsu <46119001+hokatsu@users.noreply.github.com>
2020-07-07 11:13:31 +0900

    Revert "usersテーブルにuser_idカラムを追加"
    This reverts commit 89a41de62a7d1620843914ae5689081a68e06fe0.

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2020_07_06_061016) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname"
-    t.integer "user_id"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"

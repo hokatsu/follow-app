c2d56087399c1d350bb4cf867e69a9509a54a1b2
hokatsu <46119001+hokatsu@users.noreply.github.com>
2020-07-07 17:12:47 +0900

    relationshipsコントローラへのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tweets#index'
   resources :tweets, only: [:index, :new, :create]
-  resources :users, only: :show
+
+  resources :users, only: :show do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member 
+  end
 end
+# , only: :show

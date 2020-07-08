d2c8a403320bbab691fb87b2ba730c49d2bfa7cb
hokatsu <46119001+hokatsu@users.noreply.github.com>
2020-07-08 17:45:03 +0900

    find_byをfindに変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,7 +6,7 @@ class RelationshipsController < ApplicationController
   end
 
   def destroy
-    follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
+    follow = current_user.active_relationships.find(follower_id: params[:user_id])
     follow.destroy
     redirect_to root_path
   end

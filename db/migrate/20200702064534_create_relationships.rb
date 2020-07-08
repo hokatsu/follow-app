18ccf5cf6a975a24c8d26513e28f36f1950c752e
hokatsu <46119001+hokatsu@users.noreply.github.com>
2020-07-08 15:27:24 +0900

    カラムの型をinteger型からreferences型へ変更

--- db/migrate/20200702064534_create_relationships.rb
+++ db/migrate/20200702064534_create_relationships.rb
@@ -1,8 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[6.0]
   def change
     create_table :relationships do |t|
-      t.integer :following_id
-      t.integer :follower_id
+      t.references :following
+      t.references :follower
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_064534) do
+ActiveRecord::Schema.define(version: 2020_07_03_073609) do
 
   create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "following_id"
-    t.integer "follower_id"
+    t.bigint "following_id"
+    t.bigint "follower_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["following_id"], name: "index_relationships_on_following_id"
   end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

eac2f0473c93457abec72492b01e6fb88fc0226d
hokatsu <46119001+hokatsu@users.noreply.github.com>
2020-07-16 16:23:35 +0900

    不要なファイルや行を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
   has_many :followers, through: :passive_relationships, source: :following
  
-
   def followed_by?(user)
     passive_relationships.find_by(following_id: user.id).present?
   end
